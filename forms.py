139da80cec97cd5944beb56622b35daa0ab63700
Pradbabu <pradheap@gmail.com>
2012-10-12 12:15:32 -0700

    Clean up

--- forms.py
+++ forms.py
@@ -12,18 +12,14 @@ class UserProfileForm(forms.ModelForm):
 class EmployerForm(forms.ModelForm):
     class Meta:
         model = Employer
-        exclude = ('employer','user',)
+        exclude = ('employer','address','user',)
+  
+class ProfessorForm(forms.ModelForm):
+    class Meta:
+        model = Professor
+        exclude = ('professor','address','user',)
         
 class StudentForm(forms.ModelForm):
     class Meta:
         model = Student
         exclude = ('student','user','skills','address','study')
-        
-# This we don't need in case we are doing multi table inheritance rather than one-one       
-#RegistrationForm.base_fields.update(UserProfileForm.base_fields)
-#RegistrationForm.base_fields.update(StudentForm.base_fields)
-
-#class CustomRegistrationForm(RegistrationForm):
-    #first_name = forms.CharField(label="First Name:",help_text='')
-    #last_name = forms.CharField(label="Last Name:",help_text='')
-    #reg_no = forms.CharField(label="Reg No:",help_text='')
